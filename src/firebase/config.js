e78f56b88e69f226f72bc86dac410d3052a63cb0
kevokl <lacartax@gmail.com>
2023-02-05 22:03:37 -0600

    Creando login con firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import{ getFirestore } from "firebase/firestore"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyApsRRq3rbsCMeiE6wuaBoUDnlKSogQeuA",
@@ -16,3 +18,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import LoginScreen from "../components/LoginScreen/LoginScreen"
-
+import RegisterScreen from "../components/registerScreen/RegisterScreen"
 
 
 const PublicRoutes = () => {
@@ -8,6 +8,7 @@ const PublicRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={ <LoginScreen /> }/>
+            <Route path="/register" element={ <RegisterScreen /> }/>
             <Route path="*" element={ <Navigate to="/login"/>} />
         </Routes>
     )
